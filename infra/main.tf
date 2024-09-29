1995645344ec8eb143387d50cab34a34d2538dc2
jamcatbiz <james.cate.biz@gmail.com>
2024-09-29 11:33:23 -0400

    fix(infra): adding website config

--- infra/main.tf
+++ infra/main.tf
@@ -40,3 +40,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     }
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
